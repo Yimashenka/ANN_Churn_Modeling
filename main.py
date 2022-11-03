2a156093d18656d769644f099a222d45f398374a
Yimyklkru <110557667+Yimyklkru@users.noreply.github.com>
2022-11-03 15:46:34 +0100

    Data Preprocessing

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 #Artificial Neural Network
 
-# Step 1 : Importing the libraries
+## Step 1 : Importing the libraries
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+## Step 2 :  Data Preprocessing
+### Importing the dataset
+dataset = pd.read_csv('data/Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
+### Encoding categorical data
+#### Encoding the "Gender" column
+le = LabelEncoder
+X[:, 2] = le.fit_transform(X[, :2])
+
+#### One Hot Encoding the "Geography" column
+ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [1])],
+    remainder='passthough'
+)
+X = np.array(ct.fit_transform(X))
+
+### Splitting the dataset into the training set and the test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=0
+)
+
+### Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
